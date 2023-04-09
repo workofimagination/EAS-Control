b87cf96b718d8e68cb8f906f361a7c19f222ff4d
asphergers <carter44354@gmail.com>
2023-04-08 22:30:30 -0400

    added function for turing an image fully black and white

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-use image::{ self };
+use image::{ self, imageops::{resize, FilterType::Nearest}, ImageBuffer, Rgba };
 
 fn main() {
-    let img = image::open("/home/anthonyb/projects/eas/EAS-Backend-Server/upload/main.jpeg").unwrap().to_rgba8();
+    let mut img = image::open("/home/anthonyb/projects/eas/EAS-Backend-Server/upload/main.jpeg").unwrap().to_rgba8();
+    resize(&mut img, 100, 100, Nearest);
+    img = black_and_white(&img);
 
-    
-    for pix in img.pixels() {
-        if pix[0] < 255 {
-            println!("{}", pix[1]);
+    img.save("temp.jpeg").unwrap();
+}
+
+fn black_and_white(image: &ImageBuffer<Rgba<u8>, Vec<u8>>) -> ImageBuffer<Rgba<u8>, Vec<u8>>{
+    let mut image = image.clone();
+
+    for pix in image.pixels_mut() {
+        if pix[0] < 100 {
+            for i in 0..3 { 
+                pix[i] = 0
+            }
+        } else {
+            for i in 0..3 {
+                pix[i] = 255
+            }
         }
     }
+    
+    return image;
 }
