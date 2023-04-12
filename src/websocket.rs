e320c1165810c5093262c9474ed72f6130bf814e
asphergers <carter44354@gmail.com>
2023-04-12 15:27:30 -0400

    that last commit was a lie :P. this is the final state

--- src/websocket.rs
+++ src/websocket.rs
@@ -2,6 +2,7 @@ use websocket::sync::Client;
 use websocket::OwnedMessage;
 use websocket::receiver::Reader;
 use websocket::sender::Writer;
+use websocket::header::{Headers, Header};
 
 pub struct WebsocketClient {
     recv: Reader<std::net::TcpStream>,
@@ -10,9 +11,13 @@ pub struct WebsocketClient {
 
 impl WebsocketClient{
     pub fn initalize() -> WebsocketClient {
-        let server = "ws:/0.0.0.0:3001";
+        let server = "ws:/localhost:3001";
+        let mut headers = Headers::new();
+        headers.set_raw("name", vec![b"control".to_vec()]);
+
         let ws_client = websocket::ClientBuilder::new(server)
             .unwrap()
+            .custom_headers(&headers)
             .connect_insecure()
             .unwrap();
 
